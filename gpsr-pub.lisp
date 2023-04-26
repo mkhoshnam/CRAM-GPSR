ad3905b6566e1586c39e84d153d357267a845e8d
MehreenNaeem <106755148+MehreenNaeem@users.noreply.github.com>
2023-04-26 13:03:25 +0200

    Delete gpsr-pub.lisp

--- gpsr-pub.lisp
+++
@@ -1,9 +0,0 @@
-(in-package :demo)
-(defun cram-talker (command)
-  "Periodically print a string message on the /chatter topic"
-    (let ((pub (roslisp:advertise "gpsrpub" "std_msgs/String")))
-      
-         (roslisp:publish-msg pub :data (format nil command))))
-
-         
-
