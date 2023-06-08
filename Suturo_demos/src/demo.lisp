e7b2f06ce82648dd294b0671593ff1bad16ccbb9
MehreenNaeem <106755148+MehreenNaeem@users.noreply.github.com>
2023-06-08 15:33:41 +0200

    Delete Suturo_demos directory

--- Suturo_demos/src/demo.lisp
+++
@@ -1,3 +0,0 @@
-(in-package :su-demos)
-
-(defun demo ())
--- Suturo_demos/src/gpsr-clean-plans.lisp
+++
@@ -1,9 +0,0 @@
-(in-package :su-demos)
-
-(defun test-perception ()
-  (su-real:with-hsr-process-modules
-    (exe:perform
-     (desig:an action
-               (type detecting)
-               (object (desig:an object
-                                 (type :cerealbox)))))))
